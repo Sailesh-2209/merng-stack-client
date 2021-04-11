b2e9f6a037b06ce028414e742d7a3820fb75b36a
Sailesh <saileshr2209@gmail.com>
2021-04-11 16:52:50 +0530

    created home, login, register pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home, Login, Register } from "./pages";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
+  );
 }
 
 export default App;
