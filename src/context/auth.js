f9b84d318d7d06c535ce92eab6ff18c9cda948fd
Sailesh <saileshr2209@gmail.com>
2021-04-12 15:12:45 +0530

    added persistance to user login

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,8 +1,18 @@
 import React, { useReducer, createContext } from "react";
+import jwtDecode from "jwt-decode";
 
 const initialState = {
   user: null,
 };
+
+if (localStorage.getItem("jwtToken")) {
+  const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    initialState.user = decodedToken;
+  }
+}
 
 const AuthContext = createContext({
   user: null,
@@ -30,12 +40,14 @@ function authReducer(state, action) {
 function AuthProvider(props) {
   const [state, dispatch] = useReducer(authReducer, initialState);
   function login(userData) {
+    localStorage.setItem("jwtToken", userData.token);
     dispatch({
       type: "LOGIN",
       payload: userData,
     });
   }
   function logout() {
+    localStorage.removeItem("jwtToken");
     dispatch({
       type: "LOGOUT",
     });
